4cc547a7f71d646b4909361757b2d3cf56d79487
astriskit <harry2office@gmail.com>
2019-07-21 19:26:13 +0530

    hid https

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Introduction -
+Introduction -
 
 A full-stack application to CRUD calendar events at the outlook.com with real-time updates of the events.
--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let bodyParser = require("body-parser");
 let cookieParser = require("cookie-parser");
 let router = require("./router");
 let config = require("./config");
-let https = require("https");
+// let https = require("https");
 let http = require("http");
-let fs = require("fs");
+// let fs = require("fs");
 
 let app = express();
 app.set("view engine", "pug");
@@ -13,14 +13,14 @@ app.set("views", config.views);
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use("/", router);
-https
-  .createServer(
-    {
-      key: fs.readFileSync("./certs/server.key"),
-      cert: fs.readFileSync("./certs/server.cert")
-    },
-    app
-  )
-  .listen(config.port.https);
+// https
+//   .createServer(
+//     {
+//       key: fs.readFileSync("./certs/server.key"),
+//       cert: fs.readFileSync("./certs/server.cert")
+//     },
+//     app
+//   )
+//   .listen(config.port.https);
 
 http.createServer(app).listen(config.port.http);
